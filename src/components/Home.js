1533cfe2ff4eacc0d3a2c2140332d69a29c619fb
Somnath_Nadgire <somnathnadgire1@gmail.com>
2023-01-24 18:52:04 +0530

    new

---
+++ src/components/Home.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+  <div className="row">
+  <div className="col-lg-12 list-group-item-primary img-thumbnail ">
+      <img
+        src="https://content.jdmagicbox.com/comp/hyderabad/v6/040pxx40.xx40.110609181818.j3v6/catalogue/makemytrip-com-branch-office--banjara-hills-hyderabad-travel-agents-rwam4fu901.jpg?clr=2e2e38"
+        alt=""
+        className="w-100 h-50"
+      />
+    </div><hr/>
+    <div className="col-lg-4">
+      <img
+        src="https://wallpapercave.com/wp/wp5172699.jpg"
+        alt="Table Full of Spices"
+        className="w-100 h-100"
+      />
+    </div>
+    <div className="col-lg-4">
+      <img
+        src="https://th.bing.com/th/id/R.43ca3f3f0c06a71bfef4f4d834f3b781?rik=1oSF9deLbzoi3w&riu=http%3a%2f%2ftransitionofthoughts.com%2fwp-content%2fuploads%2f2011%2f07%2fshaniwarwada_pune.jpg&ehk=TvS8vPEUM%2fBQ0Q8aOAsFevMkn6EBfVTRBm2WqgtTmyw%3d&risl=1&pid=ImgRaw&r=0"
+        alt="Winter Landscape"
+        className="w-100"
+      />
+    </div>
+    {/* {/* <div className="col-lg-4">
+      <img
+        src="https://im.rediff.com/getahead/2017/mar/27raigad-fort-shivaji-5.jpg"
+        alt="View of the City in the Mountains"
+        className="w-100"
+      />
+    </div>
+  </div>
+</div> */}
+    </div>
+  )
+}
