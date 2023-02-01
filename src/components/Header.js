1ad344cc11864ee82eb6e92c111863e479cb8dc3
Somnath Nadgire <114397962+Somnadgire1@users.noreply.github.com>
2023-02-02 00:02:01 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,26 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import AuthDetails from './AuthDetails';
 
-export default function HeaderNav() {
+
+export default function Header({email, password}) {
   return (
-    <div>
+    <>
       <div className='bg-orange'>
-      <nav className="navbar navbar-light bg-light">
+      <nav className="navbar navbar-light bg-danger">
   <div className="container">
-    <Link className="navbar-brand" to="">
+    <Link className="navbar-brand" to='/'>
       <img src="https://logos-download.com/wp-content/uploads/2020/06/MakeMyTrip_Logo.png" alt="logo" width="200" height="60"/>
     </Link>
     <nav>
-    <Link className="navbar-brand" to='/flight'>Flights</Link>
+    <Link className="navbar-brand" to='/'>Flights</Link>
     <Link className="navbar-brand" to='/hotel'>Stays</Link>
     <Link className="navbar-brand" to='/train'>Trains</Link>
-    <Link className="navbar-brand " to='/login'>Login</Link>
-    <Link className='navbar-brand' ><AuthDetails/></Link>
+    <Link className="navbar-brand " to='/login'>{email && password ? <AuthDetails/> : <p>Login</p>}</Link>
+    {/* <Link className='navbar-brand' ></Link> */}
     </nav>
     </div>
     </nav>
     <hr/>
     </div>
-    </div>
+    </>
   )
 }
